bd29840d8fdb3f494ec92d012d743c1dd7463748
ponomarevBPM <ponomarev.bpm@gmail.com>
2024-05-02 19:35:57 +0300

    RollBack changes

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,16 @@ import App from './app/App.tsx';
 import './index.css';
 import { ThemeProvider } from 'react-jss';
 import { theme } from './styles/theme.ts';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/:guestsName" element={<App />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,5 @@ export default defineConfig({
   server: {
     cors: false,
   },
-  base: '/WeddingDay/',
   plugins: [react()],
 });
