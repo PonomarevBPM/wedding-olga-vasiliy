141870e102fb42b45579c7f91d0fd9eb95d6d4e7
ponomarevBPM <ponomarev.bpm@gmail.com>
2024-06-10 15:12:09 +0300

    Change routes

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Routes>
-          <Route path="/:guestsName" element={<App />} />
+          <Route path="/VD/:guestsName" element={<App />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
