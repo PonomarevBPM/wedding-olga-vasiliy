a392c31834138e20aef76743c66035efa4b25a59
AAPonomarev <AAPonomarev@lanit.ru>
2025-02-18 14:48:35 +0300

    still overscroll fix...

--- src/app/WorkArea.tsx
+++ src/app/WorkArea.tsx
@@ -9,6 +9,7 @@ const useStyles = createUseStyles(() => ({
     workArea: {
         overflowY: 'scroll',
         height: '100dvh',
+        width: '100vw',
         scrollSnapType: 'y mandatory',
         userSelect: 'none'
     }
